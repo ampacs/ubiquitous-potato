dd01aa99ef49f55ec47105ec5e346b27d43797c8
VictorTomaz82 <victor.tomaz@gmail.com>
2018-01-27 16:43:40 +0000

    + added Deathplane and respawn mechanic and assets

---
+++ Assets/Scripts/Actors/DeathPlane.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour {
+
+    public Transform respawn;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            other.transform.position = respawn.position;
+        }
+    }
+}
