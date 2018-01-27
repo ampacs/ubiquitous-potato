e34384d259c603d05262e3b2c2a08df33c5a2bc3
VictorTomaz82 <victor.tomaz@gmail.com>
2018-01-27 04:19:13 +0000

    no message

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Actor {
+
+    private float movespeed;
+    private List<Keystone> keystones;
+    private List<BaseElement> elements;
+
+	// Use this for initialization
+	void Start () {
+        movespeed = 5f;
+        keystones = new List<Keystone>();
+        elements = new List<BaseElement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(movespeed*Input.GetAxis("Horizontal")*Time.deltaTime,0f, movespeed * Input.GetAxis("Vertical") * Time.deltaTime);
+	}
+}
