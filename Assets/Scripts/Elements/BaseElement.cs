7694de0441c82652275e02807649e6e0926a8168
André Santiago <andrempacsantiago@outlook.com>
2018-01-27 02:32:54 +0000

    Added basic element class

---
+++ Assets/Scripts/Elements/BaseElement.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class BaseElement : MonoBehaviour {
+
+    public enum ElementType {
+        None,Air,Earth,Fire,Water
+    }
+
+    public ElementType type { get; protected set; }
+    [SerializeField]
+    protected ElementType _type;
+
+    static public ElementType getCounterType (ElementType type) {
+        ElementType counter;
+        switch (type) {
+            case ElementType.Air:
+                counter = ElementType.Earth;
+                break;
+            case ElementType.Earth:
+                counter = ElementType.Air;
+                break;
+            case ElementType.Fire:
+                counter = ElementType.Water;
+                break;
+            case ElementType.Water:
+                counter = ElementType.Fire;
+                break;
+            default:
+                counter = ElementType.None;
+                break;
+        }
+        return counter;
+    }
+
+    public ElementType getCounterType () {
+        return BaseElement.getCounterType(type);
+    }
+
+    void Awake() {
+        type = _type;
+    }
+    void OnValidate() {
+        type = _type;
+    }
+}
