6d821167ca7231da425984d1af664332409c288e
André Santiago <andrempacsantiago@outlook.com>
2018-01-28 16:01:18 +0000

    Added player death & win game

--- Assets/Scripts/Actors/DeathPlane.cs
+++ Assets/Scripts/Actors/DeathPlane.cs
@@ -2,25 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathPlane : MonoBehaviour {
+public class DeathPlane : BaseHazard {
 
-    public Transform respawn;
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject.tag == "Player")
-        {
-            other.transform.position = respawn.position;
-        }
-    }
 }
---
+++ Assets/WinGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinGame : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            GameManager.instance.playerHasWonGame = true;
+        }
+    }
+}
