3890501c91711e6c489cbde20cbf88fc427ec72d
André Santiago <andrempacsantiago@outlook.com>
2018-02-01 20:48:00 +0000

    Applied temporary fix to fire hazard regading shielding

--- Assets/Scripts/Hazards/HazardFire.cs
+++ Assets/Scripts/Hazards/HazardFire.cs
@@ -4,9 +4,10 @@ public class HazardFire : BaseHazard {
 
     void OnTriggerStay(Collider other) {
         if (other.transform.tag.Equals("Player")) {
-            if (element == null)
-                 other.transform.GetComponent<PlayerHealthManager>().ApplyDamage(damage);
-            else other.transform.GetComponent<PlayerHealthManager>().ApplyDamage(damage, element.type);
+            //if (element == null)
+            //     other.transform.GetComponent<PlayerHealthManager>().ApplyDamage(damage);
+            //else other.transform.GetComponent<PlayerHealthManager>().ApplyDamage(damage, element.type);
+            other.transform.GetComponent<PlayerHealthManager>().ApplyDamage(damage, BaseElement.ElementType.Fire);
             triggeredByPlayer = true;
         }
         triggered = true;
